50b65494d67c32fe346a31d55e8aaf0b2e0f4504
Keshav Sharma <keshavsharma904517@gmail.com>
2026-08-12 22:27:36 +0530

    feat: add startup log to TaskTrackerApplication

--- backend/src/main/java/com/internal/tasktracker/TaskTrackerApplication.java
+++ backend/src/main/java/com/internal/tasktracker/TaskTrackerApplication.java
@@ -1,12 +1,17 @@
 package com.internal.tasktracker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskTrackerApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(TaskTrackerApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(TaskTrackerApplication.class, args);
+        logger.info("Task Tracker Application Started!");
     }
 }
